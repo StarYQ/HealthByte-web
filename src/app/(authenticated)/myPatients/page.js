65d833379a7c395ee5fda061903e0211ec5a8fde
Steveny38 <90789088+Steveny38@users.noreply.github.com>
2025-04-10 19:41:58 -0400

    deliverables
    -pull data from db
    -allow user to select individual patients
    -download data

--- src/app/(authenticated)/myPatients/page.js
+++ src/app/(authenticated)/myPatients/page.js
@@ -1,7 +1,7 @@
 import { getUser } from '@/lib/auth';
 import styles from './myPatients.module.css';
 import Navbar from '@/components/Navbar';
-
+import PatientComponent from '@/components/PatientComponent';
 export const metadata = {
   title: 'My Patients - HealthByte',
 };
@@ -12,13 +12,22 @@ export default async function MyPatients() {
   if (!user) {
     return <div className={styles.container}>Loading...</div>;
   }
+  const data = await fetch(process.env.APPLICATION_URL + '/api/getPatients', {
+    method: "GET",
+    headers:{
+        user: JSON.stringify(user)
+    } 
+  })
+  const res = await data.json()
+  console.log(res)
+
 
   return (
     <div className={styles.container}>
       <Navbar user={user} />
       <h1>My Patients</h1>
       <p><i>Patient list placeholder</i></p>
-      {/* TODO: Create API route to access patient list and use it for the My Patients page*/}
+      <PatientComponent patients={res.currPatients} ></PatientComponent>
     </div>
   );
 }
